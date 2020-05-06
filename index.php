5590a2dbb58b7367c381d997af3ae7ab85e51c6b
marceloprestes <barbosaprestes83@gmail.com>
2020-05-05 22:31:28 -0300

    Estrutura php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Curso GIT</title>
+</head>
+<body>
+    <h2>Testes no GIT</h2>
+
+
+    <?php
+    $fraseUm;
+    $fraseDois;
+    $fraseTres;
+    $frase = "Testando GIT";
+
+    $nome = "João";
+    $sobrenome =  "Barcellos";
+
+
+
+
+    echo $frase . " Seu " . $nome . $sobrenome;
+/*<---! --> git remote add origin https://github.com/marceloprestes/projetocurso-git.git
+git push -u origin master /*/
+
+
+?>
+
+
+</body>
+</html>
